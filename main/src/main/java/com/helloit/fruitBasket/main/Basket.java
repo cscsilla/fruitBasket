9d6bc7c88ba7d3f51725ab505de461860869c576
Csilla Csorvasi <csorvasicsillu@yahoo.co.uk>
2016-06-02 21:22:12 +0300

    tests added

---
+++ main/src/main/java/com/helloit/fruitBasket/main/Basket.java
@@ -0,0 +1,25 @@
+package com.helloit.fruitbasket.main;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Basket {
+
+    private List<Fruit> fruits;
+
+
+    public Basket() {
+        fruits = new ArrayList<Fruit>();
+    }
+
+    public void addFruit (final Fruit fruit) {
+
+        fruits.add(fruit);
+    }
+
+
+    public List<Fruit> getFruits() {
+        return Collections.unmodifiableList(fruits);
+    }
+}
